c3b49639c6256a7fc2b3e1dd151e369cc3fad7c3
Bulat Ibragimov <whoisbulat@mail.ru>
2024-01-10 20:37:11 +0300

    а

--- burger.py
+++ burger.py
@@ -1,7 +1,7 @@
 from typing import List
 
-from praktikum.bun import Bun
-from praktikum.ingredient import Ingredient
+from bun import Bun
+from ingredient import Ingredient
 
 
 class Burger:
--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from praktikum.bun import Bun
-from praktikum.ingredient import Ingredient
-from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+from bun import Bun
+from ingredient import Ingredient
+from ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 
 class Database:
